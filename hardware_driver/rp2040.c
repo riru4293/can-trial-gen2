a0069e20c2cff136dfe9912a1e64138aeab42b1e
riru4293 <41512570+riru4293@users.noreply.github.com>
2024-09-21 23:02:44 +0900

    split header

--- hardware_driver/rp2040.c
+++ hardware_driver/rp2040.c
@@ -7,44 +7,8 @@
 
 /* Driver */
 #include <private/rp2040.h>
+#include <private/rp2040_gpio.h>
 #include <private/can_irq.h>
-
-/* -------------------------------------------------------------------------- */
-/* Macro                                                                      */
-/* -------------------------------------------------------------------------- */
-/* GPIO status */
-#define GPIO_LEVEL_LOW      ( false )
-#define GPIO_LEVEL_HIGH     ( true )
-
-/* GPIO pin number */
-#define GPIO_01             ( (uint) 1U )
-#define GPIO_02             ( (uint) 2U )
-#define GPIO_03             ( (uint) 3U )
-#define GPIO_04             ( (uint) 4U )
-#define GPIO_05             ( (uint) 5U )
-#define GPIO_06             ( (uint) 6U )
-#define GPIO_07             ( (uint) 7U )
-#define GPIO_08             ( (uint) 8U )
-#define GPIO_09             ( (uint) 9U )
-#define GPIO_10             ( (uint)10U )
-#define GPIO_11             ( (uint)11U )
-#define GPIO_12             ( (uint)12U )
-#define GPIO_13             ( (uint)13U )
-#define GPIO_14             ( (uint)14U )
-#define GPIO_15             ( (uint)15U )
-#define GPIO_16             ( (uint)16U )
-#define GPIO_17             ( (uint)17U )
-#define GPIO_18             ( (uint)18U )
-#define GPIO_19             ( (uint)19U )
-#define GPIO_20             ( (uint)20U )
-#define GPIO_21             ( (uint)21U )
-#define GPIO_22             ( (uint)22U )
-#define GPIO_23             ( (uint)23U )
-#define GPIO_24             ( (uint)24U )
-#define GPIO_25             ( (uint)25U )
-#define GPIO_26             ( (uint)26U )
-#define GPIO_27             ( (uint)27U )
-#define GPIO_28             ( (uint)28U )
 
 /* SPI hardware */
 #define SPI_1_HW            ( spi0 )
